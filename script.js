0b0d8632d8629571b54a0a826e40a91310b9697f
BBDove1234 <bbdove1234@gmail.com>
2026-06-25 11:17:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const apiKey = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=";
-
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -7,7 +6,7 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city){
   let response = "";
   
-// City Finding
+  // City Finding
   if (city == "London" || city == "Moscow" || city == "Berlin") {
     response = await fetch(apiKey + "Europe%2F" +city);
     }
@@ -23,41 +22,37 @@ async function checkWeather(city){
   else if (city == "Sydney") {
     response = await fetch(apiKey + "Australia%2F" +city);
   }
-  
-  // If invalid response:
-  if (response == `"error":true,"reason":"Invalid timezone"`) {
+  else {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   }
-  else {
-    var data = await response.json();
+  
+  var data = await response.json();
     
-    document.querySelector(".city").innerHTML = data.timezone;
-    document.querySelector(".temp").innerHTML = Math.round(data.current.temperature_2m) + "°c";
-    document.querySelector(".humidity").innerHTML = data.current.relative_humidity_2m + "%";
-    document.querySelector(".wind").innerHTML = data.current.wind_speed_10m + " km/h";
+  document.querySelector(".city").innerHTML = data.timezone;
+  document.querySelector(".temp").innerHTML = Math.round(data.current.temperature_2m) + "°c";
+  document.querySelector(".humidity").innerHTML = data.current.relative_humidity_2m + "%";
+  document.querySelector(".wind").innerHTML = data.current.wind_speed_10m + " km/h";
     
-    // Weather code
-    if(data.weather_code == 0) {
-      weatherIcon.src = "images/clear.png";
-    }
-    else if (data.weather_code == 1 || data.weather_code == 2 || data.weather_code == 3) {
-      weatherIcon.src = "images/clouds.png";
-    }
-    else if (data.weather_code == 51 || data.weather_code == 53 || data.weather_code == 55) {
-      weatherIcon.src = "images/drizzle.png";
-    }
-    else if (data.weather_code == 61 || data.weather_code == 63 || data.weather_code == 65 || data.weather_code == 80 || data.weather_code == 81 || data.weather_code == 82) {
-      weatherIcon.src = "images/rain.png;"
-    }
-    
-    document.querySelector(".weather").style.display = "block"
-    document.querySelector(".error").style.display = "none";
+  // Weather code
+  if(data.weather_code == 0) {
+    weatherIcon.src = "images/clear.png";
   }
+  else if (data.weather_code == 1 || data.weather_code == 2 || data.weather_code == 3) {
+    weatherIcon.src = "images/clouds.png";
+  }
+  else if (data.weather_code == 51 || data.weather_code == 53 || data.weather_code == 55) {
+    weatherIcon.src = "images/drizzle.png";
+  }
+  else if (data.weather_code == 61 || data.weather_code == 63 || data.weather_code == 65 || data.weather_code == 80 || data.weather_code == 81 || data.weather_code == 82) {
+    weatherIcon.src = "images/rain.png;"
+  }
+    
+  document.querySelector(".weather").style.display = "block"
+  document.querySelector(".error").style.display = "none";
 
 }
 
 searchBtn.addEventListener("click", ()=>{
   checkWeather(searchBox.value);
 })
-
